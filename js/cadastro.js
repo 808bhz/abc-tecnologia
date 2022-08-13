3579f879919f31292f760c412033acab1924d4d4
808bhz <107586803+808bhz@users.noreply.github.com>
2022-08-12 22:32:32 -0300

    Publicação inicial

---
+++ js/cadastro.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/* Selecionando os campos do formulário */
+const formulario = document.querySelector("form");
+const campoNome = document.querySelector("#nome");
+const campoData = document.querySelector("#data");
+const campoTelefone = document.querySelector("#telefone");
+const campoEmail = document.querySelector("#email");
+const campoVaga = document.querySelector("#vaga");
+
+/* Programação de evento do formulário */
+formulario.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    /* Montando um objeto JavaScript com os 
+    valores capturados dos campos do formulário */
+    let dados = {
+        nome : campoNome.value,
+        data : campoData.value,
+        telefone : campoTelefone.value,
+        email : campoEmail.value,
+
+        /* A instrução abaixo permite
+        pegar o título (textContent) apenas
+        da vaga que foi selecionada (selectOptions[0])
+        dentro da lista de vagas (campoVaga) */
+        vaga : campoVaga.selectedOptions[0].textContent
+    };
+    
+
+    /* Ajax (técnica de comunicação)
+    Endpoint de candidatos */
+
+    /* 1) Acessar/Conectar à API */
+    fetch('http://localhost:3000/candidatos', {
+        
+        // Definimos o método de transmissão dos dados
+        method: "POST", // POST envia, GET recebe (padrão)
+        
+        // Convertemos o objeto JS em objeto JSON
+        body: JSON.stringify(dados),
+
+        // Indicamos no cabeçalho da mensagem o formato de dados
+        headers: { "Content-type" : "application/json" }
+    })
+    
+    /* 2) Resposta da API transformada em JSON */
+    .then(resposta => resposta.json())
+    
+    /* 3) Exibindo mensagem final do processo */
+    .then( () => alert("Dados cadastrados com sucesso!") );
+});
+
